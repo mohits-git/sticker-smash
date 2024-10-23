8504546842f5e16c8980fa3c02101666143617ed
mohits-git <mohitkhatri2805@gmail.com>
2024-10-23 10:54:39 +0530

    reset: reset-application and clean up

---
+++ app/index.tsx
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={styles.container}
+    >
+      <Text style={styles.text}>Hello, World!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+  },
+});
