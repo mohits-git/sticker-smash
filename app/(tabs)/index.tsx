c378541a1b8cd6f944b731c8a4987d4d8e945bb9
mohits-git <mohitkhatri2805@gmail.com>
2024-10-26 22:06:22 +0530

    feat: wireup imageviewer and buttons and add image picker logic

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,41 @@
-import { Link } from "expo-router";
-import { StyleSheet, Text, View } from "react-native";
+import Button from "@/components/Button";
+import ImageViewer from "@/components/ImageViewer";
+import { StyleSheet, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+
+const PlaceHolderImage = require("@/assets/images/background-image.png");
 
 export default function Index() {
+  const [selectedImage, setSelectedImage] = useState<
+    { uri: string } | undefined
+  >();
+
+  const pickImageAsync = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+    });
+    if (result.canceled) {
+      alert("You didn't select any image.");
+    } else {
+      setSelectedImage({ uri: result.assets[0].uri });
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello, World!</Text>
-      <Link href={"/about"} style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={selectedImage ?? PlaceHolderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button
+          label="Choose a image"
+          theme="primary"
+          onPress={pickImageAsync}
+        />
+        <Button label="Use this image" />
+      </View>
     </View>
   );
 }
@@ -19,12 +47,11 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  text: {
-    color: "#fff",
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    color: "#fff",
-    fontSize: 20,
-    textDecorationLine: "underline",
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: "center",
   },
 });
